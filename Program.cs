a6da19149adb24f501db8bc2cabcd16063da59de
Florian Ibach <fibach@grenke.de>
2023-08-29 10:49:57 +0200

    basic working implementation of QR code with logo

--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
 using QRCoder;
 using static QRCoder.PayloadGenerator;
+using static QRCoder.SvgQRCode;
 
 Console.WriteLine("Hello, World!");
 
 
-var generator = new Url("https://github.com/codebude/QRCoder/");
-var payload = generator.ToString();
+var url = new Url("https://youtu.be/gkDRHvjcypY?si=PCq-KjOYrxA8n7ir");
 
 var qrGenerator = new QRCodeGenerator();
-var qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+var qrCodeData = qrGenerator.CreateQrCode($"{url}", QRCodeGenerator.ECCLevel.Q);
 var qrCode = new SvgQRCode(qrCodeData);
-var qrCodeAsBitmap = qrCode.GetGraphic(20);
+var svr = await File.ReadAllBytesAsync(@"D:\temp\jer.svg");
+var bitmap = new Bitmap(@"D:\temp\jer_stift.png");
+var svgLogo = new SvgLogo(bitmap, iconSizePercent: 50, fillLogoBackground: false);
+var qrCodeAsImage = qrCode.GetGraphic(20, Color.White, Color.Transparent, drawQuietZones: true, logo: svgLogo);
+await WriteToFileAsync(qrCodeAsImage);
+
+Console.WriteLine("finished");
+Console.ReadKey();
+
+static async Task WriteToFileAsync(string qrCode)
+{
+    await using var outputFile = new StreamWriter(Path.Combine(@"D:\share", "QrCode.svg"));
+    await outputFile.WriteAsync(qrCode);
+}
