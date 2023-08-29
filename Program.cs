5559178cadc5be6201ddc01cf99457286a375f17
Florian Ibach <fibach@grenke.de>
2023-08-29 09:36:00 +0200

    adding gitignore

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QRCoder;
+using static QRCoder.PayloadGenerator;
+
+Console.WriteLine("Hello, World!");
+
+
+var generator = new Url("https://github.com/codebude/QRCoder/");
+var payload = generator.ToString();
+
+var qrGenerator = new QRCodeGenerator();
+var qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+var qrCode = new SvgQRCode(qrCodeData);
+var qrCodeAsBitmap = qrCode.GetGraphic(20);
